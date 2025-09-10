5a43273033465519628d553bd012bc3a7daa1fee
bernardomiranda8 <bernardo.rocha@escolar.ifrn.edu.br>
2025-09-10 17:50:55 -0300

    update em media.py

---
+++ media.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite um número:"))
+n2 = int(input("Outro número:"))
+n3 = int(input("Outro número:"))
+n4 = int(input("Outro número:"))
+media = (n1+n2+n3+n4)/4
+print(media)
---
+++ numero.py
@@ -0,0 +1,4 @@
+n1 = int(input("Digite um número:"))
+n2 = int(input("Outro número:"))
+soma = n1+n2
+print(soma)
